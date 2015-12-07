d8afc91d01856a39b6617958d0abc6c03d709be1
Stephen Bunch <bunchclone@gmail.com>
2015-12-07 14:25:25 -0800

    Update content negotation

--- src/server/middleware/ContentNegotiation.js
+++ src/server/middleware/ContentNegotiation.js
@@ -6,11 +6,7 @@ function( StatusCode, Json ) {
       // TODO: Return different results based on Accept header.
       var result = await next.invokeAsync( request );
       if ( result !== undefined ) {
-        if ( result === null ) {
-          result = new StatusCode( 404 );
-        } else if ( typeof result === 'number' ) {
-          result = new StatusCode( result );
-        } else if ( result.isContent ) {
+        if ( result.isContent ) {
           if ( result.hasBody ) {
             return new Json( result.statusCode, result.body, result.headers );
           } else {
