53fd584adbd27059a60461216c24f97fe5fe2b10
Stephen Bunch <bunchclone@gmail.com>
2015-12-16 08:59:12 -0800

    Update walkChildren to flatten children

--- src/common/walkChildren.js
+++ src/common/walkChildren.js
@@ -1,15 +1,6 @@
 export default [ 'react', 'immutable', function( React, Immutable ) {
-  function makeArray( children ) {
-    if ( Immutable.List.isList( children ) ) {
-      return children.toArray();
-    } else if ( Array.isArray( children ) ) {
-      return children.slice();
-    } else {
-      return [ children ];
-    }
-  }
   return function( children, callback ) {
-    children = makeArray( children );
+    children = flatten( children );
     var search = children.reverse();
     while ( search.length > 0 ) {
       let element = search.pop();
@@ -20,8 +11,28 @@ export default [ 'react', 'immutable', function( React, Immutable ) {
         if ( callback( element ) === false ) {
           break;
         }
-        search = makeArray( element.props.children ).reverse().concat( search );
+        search = flatten( element.props.children ).reverse().concat( search );
       }
     }
   };
+
+  function makeArray( children ) {
+    if ( Immutable.List.isList( children ) ) {
+      return children.toArray();
+    } else if ( Array.isArray( children ) ) {
+      return children.slice();
+    } else {
+      return [ children ];
+    }
+  }
+
+  function flatten( children ) {
+    return makeArray( children ).reduce( ( list, child ) => {
+      if ( Immutable.List.isList( child ) || Array.isArray( child ) ) {
+        return list.concat( flatten( child ) );
+      } else {
+        return list.concat([ child ]);
+      }
+    }, [] );
+  }
 }];
