77cdf4e32883bf83e7ebc19dafb221ed4cbfc18b
Stephen Bunch <bunchclone@gmail.com>
2016-07-06 19:54:33 -0700

    Also redirect if hostname doesn't match

--- src/server/middleware/HttpsRedirect.js
+++ src/server/middleware/HttpsRedirect.js
@@ -5,7 +5,10 @@ export default [ 'result/StatusCode', function( StatusCode ) {
     }
 
     async invokeAsync( request, next ) {
-      if ( request.headers['x-forwarded-proto'] !== 'https' ) {
+      if (
+        request.headers['x-forwarded-proto'] !== 'https' ||
+        request.headers['host'] !== this._hostname
+      ) {
         return new StatusCode( 301, {
           'Location': `https://${ this._hostname }${ request.url }`
         });
